2437e4cb2c7d531bf77ca361b625144cb1b3bcd8
Max Otta <max.otta@gmail.com>
2024-10-18 02:55:40 +0200

    Further improvements. Ansible username and password can be passed as environment variables.

--- init-dev-container.sh
+++ init-dev-container.sh
@@ -5,6 +5,10 @@ test -f /root/.ssh/id_ecdsa || ln -sf ${PERSISTENT_DATA_DIR}/id_ecdsa /root/.ssh
 export LC_ALL="C"
 export PATH=$PATH:/usr/games
 
+set -a
+source ${ENV_CONFIG}
+set +a
+
 alias ppk="cat ${PERSISTENT_DATA_DIR}/id_ecdsa.pub"
 alias vpn-status='(ping -q -w 2 -c 3 gw >/dev/null && echo "VPN is up.") || echo "VPN is down."'
 alias vpn-up="openvpn --config ${OPENVPN_CONFIG} --daemon ; sleep 5 ; vpn-status"
